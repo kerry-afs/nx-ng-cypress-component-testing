64e68c8877a004c4282960fd7f0b43c1f9397e74
Kerry McKeever <kerry.mckeever@apprenticefs.com>
2022-10-21 16:20:37 -0400

    Initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ng-cypress-component-testing-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
